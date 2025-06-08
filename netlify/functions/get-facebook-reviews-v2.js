2a67496c3bfd823fd05c0a69e4568b1b13054b8a
sussexsofas <info@sussexsofas.com>
2025-06-08 22:23:19 +0100

    Shopify Facebook Reviews
    For use on Netlify

---
+++ netlify/functions/get-facebook-reviews-v2.js
@@ -0,0 +1,44 @@
+exports.handler = async function (event, context) {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
+  const PAGE_ID = 'YOUR_PAGE_ID';
+  const ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN';
+  const url = `https://graph.facebook.com/v19.0/${PAGE_ID}/ratings?access_token=${ACCESS_TOKEN}`;
+
+  try {
+    const fbRes = await fetch(url);
+    const fbJson = await fbRes.json();
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(fbJson.data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, OPTIONS',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ error: 'Facebook fetch failed', details: err.message })
+    };
+  }
+};
