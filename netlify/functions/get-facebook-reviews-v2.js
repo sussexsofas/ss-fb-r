e62b28f27d8a589b4868ca9115bb1c6a6d8e2ec0
sussexsofas <info@sussexsofas.com>
2025-06-10 15:48:02 +0100

    Updated V2
    Key

--- netlify/functions/get-facebook-reviews-v2.js
+++ netlify/functions/get-facebook-reviews-v2.js
@@ -12,7 +12,7 @@ exports.handler = async function (event, context) {
   }
 
   const PAGE_ID = '61552186662245';
-  const ACCESS_TOKEN = 'EAAKfNyEtz7cBO9NEn4UE3bYxothl2sVWe5vKaNzT5KwLy5JhFQnqgbQxy1g3D89ZB0cdhiEXoFhpWfxLeWsHSSnOgOKkwhY0chaNZB0gTfdAcOhB8JBidPWFsGyGEAnl3V4ewoGSZA0C7AVTsuUuN9pFkVvVNCHWNnVUr7PnQKw5gujKNHZAqjcZBVqtUIZBbtcEZBSRVWkT9AxlZCdm2sDGWCIXg8MZD';
+  const ACCESS_TOKEN = 'EAAKfNyEtz7cBO2yplFZCou8JxHI3Pn6DCtcU0JEKEfuG1QcOOHveE1LIXav8aavr7yghaUdmbyZAuaTym8Yt38LSgGyFbX7PvQo3T32lN6pSMZAIaPs2DPdMA2FBErj08eGYj1zMeWzItPlZAtnZBFpZAJ2MOk51QLz7JRySN4VLUGZAz1dWY5KdoXBXtFYise4i66EgY3ow1Tjyio7H3Az009iX91T';
   const url = `https://graph.facebook.com/v19.0/${PAGE_ID}/ratings?access_token=${ACCESS_TOKEN}`;
 
   try {
