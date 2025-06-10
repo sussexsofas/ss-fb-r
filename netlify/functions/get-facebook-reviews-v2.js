7144d878947bbdcf3b33713a833389594f7a64b1
sussexsofas <info@sussexsofas.com>
2025-06-10 15:43:08 +0100

    New Keys
    Upload

--- netlify/functions/get-facebook-reviews-v2.js
+++ netlify/functions/get-facebook-reviews-v2.js
@@ -11,8 +11,8 @@ exports.handler = async function (event, context) {
     };
   }
 
-  const PAGE_ID = '470026866204994';
-  const ACCESS_TOKEN = 'EAARvxYSNLa8BO99BaMrWQDo8A6tC0xgBLjsyzALjGyw8ZAiuy1wj7A8SzUDZCIaXN5cN2jPBnbxQhU9GwXjUZAtYwHlnN0ocaWOjNWvZC915MRVxgb3owvqsrXZASkpDMVi1Gls5iCzuaEABDaECoh2btM0xDeCRhtnxFAj15nZCgyiPY0CxiVJZCc09lxLTUoVZClEY4rGe8fIDZA55caZC2WARoMWdTX';
+  const PAGE_ID = '61552186662245';
+  const ACCESS_TOKEN = 'EAAKfNyEtz7cBO9NEn4UE3bYxothl2sVWe5vKaNzT5KwLy5JhFQnqgbQxy1g3D89ZB0cdhiEXoFhpWfxLeWsHSSnOgOKkwhY0chaNZB0gTfdAcOhB8JBidPWFsGyGEAnl3V4ewoGSZA0C7AVTsuUuN9pFkVvVNCHWNnVUr7PnQKw5gujKNHZAqjcZBVqtUIZBbtcEZBSRVWkT9AxlZCdm2sDGWCIXg8MZD';
   const url = `https://graph.facebook.com/v19.0/${PAGE_ID}/ratings?access_token=${ACCESS_TOKEN}`;
 
   try {
